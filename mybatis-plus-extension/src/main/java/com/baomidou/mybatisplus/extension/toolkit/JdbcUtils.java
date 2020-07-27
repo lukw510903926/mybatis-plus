39cf92bde822192468c1b30bc5c926c2afd4a498
lkw510903926 <lkw510903926@163.com>
2020-07-27 15:56:47 +0800

    Initial commit

---
+++ mybatis-plus-extension/src/main/java/com/baomidou/mybatisplus/extension/toolkit/JdbcUtils.java
@@ -0,0 +1,79 @@
+/*
+ * Copyright (c) 2011-2020, baomidou (jobob@qq.com).
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * <p>
+ * https://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.baomidou.mybatisplus.extension.toolkit;
+
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.core.toolkit.Assert;
+import com.baomidou.mybatisplus.core.toolkit.StringUtils;
+import org.apache.ibatis.logging.Log;
+import org.apache.ibatis.logging.LogFactory;
+
+/**
+ * JDBC 工具类
+ *
+ * @author nieqiurong
+ * @since 2016-12-05
+ */
+public class JdbcUtils {
+
+    private static final Log logger = LogFactory.getLog(JdbcUtils.class);
+
+    /**
+     * 根据连接地址判断数据库类型
+     *
+     * @param jdbcUrl 连接地址
+     * @return ignore
+     */
+    public static DbType getDbType(String jdbcUrl) {
+        Assert.isFalse(StringUtils.isBlank(jdbcUrl), "Error: The jdbcUrl is Null, Cannot read database type");
+        String url = jdbcUrl.toLowerCase();
+        if (url.contains(":mysql:") || url.contains(":cobar:")) {
+            return DbType.MYSQL;
+        } else if (url.contains(":mariadb:")) {
+            return DbType.MARIADB;
+        } else if (url.contains(":oracle:")) {
+            return DbType.ORACLE;
+        } else if (url.contains(":sqlserver:") || url.contains(":microsoft:")) {
+            return DbType.SQL_SERVER2005;
+        } else if (url.contains(":sqlserver2012:")) {
+            return DbType.SQL_SERVER;
+        } else if (url.contains(":postgresql:")) {
+            return DbType.POSTGRE_SQL;
+        } else if (url.contains(":hsqldb:")) {
+            return DbType.HSQL;
+        } else if (url.contains(":db2:")) {
+            return DbType.DB2;
+        } else if (url.contains(":sqlite:")) {
+            return DbType.SQLITE;
+        } else if (url.contains(":h2:")) {
+            return DbType.H2;
+        } else if (url.contains(":dm:")) {
+            return DbType.DM;
+        } else if (url.contains(":xugu:")) {
+            return DbType.XU_GU;
+        } else if (url.contains(":kingbase:") || url.contains(":kingbase8:")) {
+            return DbType.KINGBASE_ES;
+        } else if (url.contains(":phoenix:")) {
+            return DbType.PHOENIX;
+        } else if (jdbcUrl.contains(":zenith:")) {
+            return DbType.GAUSS;
+        } else {
+            logger.warn("The jdbcUrl is " + jdbcUrl + ", Mybatis Plus Cannot Read Database type or The Database's Not Supported!");
+            return DbType.OTHER;
+        }
+    }
+
+}
